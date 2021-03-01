04c59bc8add43c6837e25db4b830110f548e3344
Muntasir Biojid <muntasir2165@hotmail.com>
2021-03-01 03:11:12 -0700

    4-redux setup and api middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import '@fortawesome/fontawesome-free/js/all';
 
 import './includes/bootstrap';
 import './index.css';
 import App from './App';
+import configureStore from './redux/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
